9a9465416373916be46842041c3e61d76750edd3
Daria D <darja.dolbe@gmail.com>
2020-06-03 20:28:18 +0300

    Basic things

--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,43 @@ using System.Windows.Forms;
 
 namespace TrafficLights
 {
-    public partial class Form1 : Form
+    public partial class TrafficLights : Form
     {
-        public Form1()
+
+        private Timer timerSwitch;
+
+
+        public TrafficLights()
         {
             InitializeComponent();
+            InitializeTrafficLights();
+            InitializeTimerSwitch();
+        }
+
+        private void InitializeTimerSwitch()
+        {
+            timerSwitch = new Timer();
+            timerSwitch.Interval = 1000;]
+            timerSwitch.Tick += new EventHandler(TimerSwitch_Tick);
+        }
+
+        private void TimerSwitch_Tick (object sender, EventArgs e)
+        {
+            if (RedLight.BackColor == Color.DarkGray)
+            {
+                RedLight.BackColor = Color.Red;
+            }
+            else
+            {
+                RedLight.BackColor = Color.DarkGray;
+            }
+        }
+
+        private void InitializeTrafficLights()
+        {
+            RedLight.BackColor = Color.DarkGray;
+            YellowLight.BackColor = Color.DarkGray;
+            GreenLight.BackColor = Color.DarkGray;
         }
     }
 }
